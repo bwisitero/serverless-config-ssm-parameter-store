d6171fadef93d908cfefe1f7340c8df89c534246
Franklin Aguinaldo <franklin@plumlending.com>
2018-03-12 01:55:21 -0700

    configured serverless to use stage environment

--- handlers/handler.js
+++ handlers/handler.js
@@ -2,22 +2,28 @@
 const co           = require('co');
 const configClient = require('../lib/configClient');
 
-const config1 = configClient.loadConfigs(
-  [ "testparameter", "/dev/database/mssql/connectionstring/json" ], 
-  30000); // cache config values for 30s
+
+
+
 
 module.exports.hello = co.wrap(function* (event, context, callback) {
-
+  console.log('stage ', process.env.STAGE)
+  console.log('testparameter ', process.env.TEST_PARAMETER)
+  console.log('connectionstring ', process.env.CONNECTION_STRING)
+  const config1 = configClient.loadConfigs(
+    [ process.env.TEST_PARAMETER, process.env.CONNECTION_STRING ], 
+    30000); // cache config values for 30s
+  
   let params = [
     yield config1.testparameter,
-    yield config1["/dev/database/mssql/connectionstring/json"]
+    yield config1[process.env.CONNECTION_STRING]
   ]
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      params: [ params ],
+      params: [ params ]
     }),
   };
 
